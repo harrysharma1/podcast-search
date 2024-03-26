766dbe36820a335521949c105875c96194bb874c
Harry Sharma <harrysharma1066@gmail.com>
2024-03-26 14:44:33 +0000

    feat: add search bar

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 import spacy
-from sklearn.feature_extraction.text import TfidfVectorizer 
+from sklearn.feature_extraction.text import TfidfVectorizer
 from flask import Flask
-from flask import render_template 
+from flask import render_template, request
+from src.podcast_id import PodcastId
 app = Flask(__name__)
+
 
 video_id = "a87Tb27UE4Y"
 nlp = spacy.load("en_core_web_sm")
 
-#tfidf_vectorizer = TfidfVectorizer()
-a = YouTubeTranscriptApi.get_transcript(video_id,languages=['en'])
-#tfidf_matrix = tfidf_vectorizer.fit_transform(a)
-print(a)
-for i in a:
-    pass
-
+# tfidf_vectorizer = TfidfVectorizer()
+#  a = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
+# tfidf_matrix = tfidf_vectorizer.fit_transform(a)
+# print(a)
 
 
 @app.route("/")
-def hello_world():
+def index():
+    form = PodcastId(request.form)
     return render_template("index.html")
+
+
+@app.route("/search", methods=["GET", "POST"])
+def podcast_search():
+    form = PodcastId(request.form)
+    if request.method == "POST" and form.validate():
+        url = f"https://www.youtube.com/watch?v={form.id.data}"
+        return render_template("index.html", url=url)
+    return render_template("podcast_form.html", form=form)
---
+++ src/podcast_id.py
@@ -0,0 +1,4 @@
+from wtforms import Form, StringField
+
+class PodcastId(Form):
+    id = StringField("ID")
