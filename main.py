eaaf4afdf9775971b776f1b76ed8d9de808e4633
Harry Sharma <harrysharma1066@gmail.com>
2024-03-28 13:42:03 +0000

    feat: rework render_template to redirect to url

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 from typing import List
 from youtube_transcript_api import YouTubeTranscriptApi
 import spacy
-import time
 from sklearn.feature_extraction.text import TfidfVectorizer
 from flask import Flask
-from flask import render_template, request
+from flask import render_template, request, redirect, render_template, request, url_for
 from src.podcast_id import PodcastId
-from pytube import YouTube
-
+from src.podcast_id import PodcastSearch
+from pytube import YouTube, query
+from sklearn.metrics.pairwise import cosine_similarity
 
 app = Flask(__name__)
 app.jinja_env.filters['zip'] = zip
 
 nlp = spacy.load("en_core_web_sm")
-# tfidf_vectorizer = TfidfVectorizer()
-#  a = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
-# tfidf_matrix = tfidf_vectorizer.fit_transform(a)
-# print(a)
+tfidf_vectorizer = TfidfVectorizer()
 
 
 def conversion(seconds):
@@ -41,29 +38,25 @@ def format_time(time: List[str]):
 def index():
     form = PodcastId(request.form)
     if request.method == "POST" and form.validate():
-        url = f"{form.url.data}"
-        video_id = url.split("v=")[-1]
-        transcript_dict_list = YouTubeTranscriptApi.get_transcript(
-            video_id, languages=['en'])
-        video = YouTube(url=url)
-        video_title = video.title
-        video_thumbnail = video.thumbnail_url
-        text = []
-        start = []
-        for i in transcript_dict_list:
-            text.append(i["text"])
-            start.append(i["start"])
-        doc = nlp("This is a sentence.")
-        s = time.time()
-        for i in text:
-            doc = nlp(i)
-            print([(w.text, w.pos_) for w in doc])
-        e = time.time()
-        print(f"Tooks {e-s}s to tokenize")
-        start = format_time(start)
-        return render_template("podcast_form.html", video_title=video_title, video_thumbnail=video_thumbnail, text=text, start=start)
+        url = form.url.data
+        return redirect(url_for("search", url=url))
     return render_template("index.html", form=form)
 
 
-if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    form = request.args.get("url")
+    url = f"{form}"
+    video_id = url.split("v=")[-1]
+    transcript_dict_list = YouTubeTranscriptApi.get_transcript(
+        video_id, languages=['en'])
+    video = YouTube(url=url)
+    video_title = video.title
+    video_thumbnail = video.thumbnail_url
+    text = []
+    start = []
+    for i in transcript_dict_list:
+        text.append(i["text"])
+        start.append(i["start"])
+    start = format_time(start)
+    return render_template("podcast_form.html", video_title=video_title, video_thumbnail=video_thumbnail, start=start, text=text)
--- src/podcast_id.py
+++ src/podcast_id.py
@@ -1,5 +1,9 @@
-from wtforms import Form, URLField
+from wtforms import Form, StringField, URLField
 
 
 class PodcastId(Form):
     url = URLField("URL")
+
+
+class PodcastSearch(Form):
+    search = StringField("SEARCH")
